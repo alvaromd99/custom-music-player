841df78e3b7eae93b7c14500b8fe6ef990c3898f
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-03-22 20:24:56 +0100

    add the back ground images

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <section className='h-screen'>
-        <img src='./images/logo.jpeg' />
-        <div>
-
-        </div>
-      </section>
-    </>
-  )
+	return (
+		<div className='app'>
+			<section className='first-section h-screen'>
+				<header className='w-full p-'>
+					<img src='/src/images/logo.jpeg' className='w-24 h-24' />
+				</header>
+				<div></div>
+			</section>
+			<section className='second-section'></section>
+		</div>
+	)
 }
 
 export default App
