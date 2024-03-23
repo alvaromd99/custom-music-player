db46d4baf0023e48d9f0f6f90d860c2293e608b3
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-03-23 23:05:10 +0100

    change some properties from css to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className='font-montse-var overflow-y-scroll'>
-      <section className='first-section h-screen overflow-hidden shadow-custom-shadow'>
+      <section className='first-section h-screen overflow-hidden shadow-custom-shadow before:bg-hero-bg-image before:bg-center before:bg-cover before:absolute'>
         <header className='w-full'>
           <img src='/src/images/logo.jpeg' className='w-20 h-20' />
         </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'montse-var': ['Montserrat Variable', 'sans-serif'],
-			},
-			boxShadow: {
-				'custom-shadow': 'inset 0px -120px 30px 15px rgba(0, 0, 0, 0.75)',
-			},
-		},
-	},
-	plugins: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montse-var': ['Montserrat Variable', 'sans-serif'],
+      },
+      boxShadow: {
+        'custom-shadow': 'inset 0px -120px 30px 15px rgba(0, 0, 0, 0.75)',
+      },
+      backgroundImage: {
+        'hero-bg-image': "url('/src/images/festival-bg.avif')"
+      }
+    },
+  },
+  plugins: [],
 }
