93f7e1505c7bfdd6921c72d9064457a3b2324662
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-03-26 17:02:48 +0100

    use only tailwind for the background on the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 
 function App() {
-	return (
-		<div className=''>
-			<section className='hero-section before:bg-hero-image before:brightness-20 before:blur-sm before:-top-10 before:-left-10 before:w-120'></section>
-			<section className='content-section'></section>
-		</div>
-	)
+  return (
+    <div className=''>
+      <section className='hero-section h-screen overflow-hidden relative before:bg-hero-image before:brightness-20 before:blur-sm before:-top-10 before:-left-10 before:w-120 before:h-120 before:absolute before:bg-cover before:bg-center'></section>
+      <section className='content-section'></section>
+    </div>
+  )
 }
 
 export default App
