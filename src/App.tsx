4044670bc87a618fdb02fe797dafaa497b854146
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-03-26 15:58:03 +0100

    add customs tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import './App.css'
 
 function App() {
-  return (
-    <div className='font-montse-var overflow-y-scroll'>
-      <section className='first-section h-screen overflow-hidden shadow-custom-shadow before:bg-hero-bg-image before:bg-center before:bg-cover before:absolute'>
-        <header className='w-full'>
-          <img src='/src/images/logo.jpeg' className='w-20 h-20' />
-        </header>
-        <div></div>
-      </section>
-      <section className='second-section h-screen overflow-hidden relative'></section>
-    </div>
-  )
+	return (
+		<div className=''>
+			<section className='hero-section before:bg-hero-image before:brightness-20 before:blur-sm before:-top-10 before:-left-10 before:w-120'></section>
+			<section className='content-section'></section>
+		</div>
+	)
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        'montse-var': ['Montserrat Variable', 'sans-serif'],
-      },
-      boxShadow: {
-        'custom-shadow': 'inset 0px -120px 30px 15px rgba(0, 0, 0, 0.75)',
-      },
-      backgroundImage: {
-        'hero-bg-image': "url('/src/images/festival-bg.avif')"
-      }
-    },
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'montse-var': ['Montserrat Variable', 'sans-serif'],
+			},
+			boxShadow: {
+				'custom-shadow': 'inset 0px -120px 30px 15px rgba(0, 0, 0, 0.75)',
+			},
+			backgroundImage: {
+				'hero-image': "url('/src/images/festival-bg.avif')",
+				'content-image':
+					"background-image: url('/src/images/background-tile.webp')",
+			},
+			brightness: {
+				20: '20%',
+			},
+			width: {
+				120: '120%',
+			},
+			height: {
+				120: '120%',
+			},
+		},
+	},
+	plugins: [],
 }
