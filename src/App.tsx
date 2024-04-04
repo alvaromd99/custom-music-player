69a94c6fa5f3ecf5ec9cb1201dfaaa784b47ed21
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-04-04 13:06:44 +0200

    change bg and add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import './App.css'
+import musicLogo from './images/logo2.jpeg'
 import pageLogo from './images/page-logo.jpeg'
 
 function App() {
-	return (
-		<div className=''>
-			<section className='hero-section h-screen overflow-hidden relative before:bg-hero-image before:brightness-20 before:blur-sm before:-top-10 before:-left-10 before:w-120 before:h-120 before:absolute before:bg-cover before:bg-center'>
-				<div className='fixed top-10 left-8 w-8 h-8'>
-					<img src={pageLogo} alt='logo' />
-				</div>
-				<ul>
-					<li>Inst</li>
-				</ul>
-			</section>
-			<section className='content-section'></section>
-		</div>
-	)
+  return (
+    <div className='font-montse-var'>
+      <section className='relative flex items-center justify-center h-screen overflow-hidden hero-section before:bg-hero-image before:brightness-20 before:blur-sm before:-top-10 before:-left-10 before:w-120 before:h-120 before:absolute before:bg-cover before:bg-center before:z-neg before:shadow-custom-shadow'>
+        <div className='fixed w-8 h-8 top-10 left-8'>
+          <img src={pageLogo} alt='logo' />
+        </div>
+        <div className='flex gap-6'>
+          <div className='w-52 h-52'>
+            <img src={musicLogo} alt='music logo' />
+          </div>
+          <div className='flex flex-col justify-center gap-8'>
+            <div>
+              <h1 className='text-xl text-white'>StarJam</h1>
+              <h2 className=''>Denis</h2>
+            </div>
+            <div>
+              <button>Listen Now</button>
+              <button>Share</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='content-section'></section>
+    </div>
+  )
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'montse-var': ['Montserrat Variable', 'sans-serif'],
-			},
-			boxShadow: {
-				'custom-shadow': 'inset 0px -120px 30px 15px rgba(0, 0, 0, 0.75)',
-			},
-			backgroundImage: {
-				'hero-image': "url('/src/images/festival-bg.avif')",
-				'content-image':
-					"background-image: url('/src/images/background-tile.webp')",
-			},
-			brightness: {
-				20: '20%',
-			},
-			width: {
-				120: '120%',
-			},
-			height: {
-				120: '120%',
-			},
-		},
-	},
-	plugins: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montse-var': ['Montserrat Variable', 'sans-serif'],
+      },
+      boxShadow: {
+        'custom-shadow': 'inset 0px -120px 30px 15px rgba(0, 0, 0, 0.75)',
+      },
+      backgroundImage: {
+        'hero-image': "url('/src/images/logo2.jpeg')",
+        'content-image':
+          "background-image: url('/src/images/background-tile.webp')",
+      },
+      brightness: {
+        20: '20%',
+      },
+      width: {
+        120: '120%',
+      },
+      height: {
+        120: '120%',
+      },
+      zIndex: {
+        neg: '-1',
+      },
+    },
+  },
+  plugins: [],
 }
