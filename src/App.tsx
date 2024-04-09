8bd3e33235579e571cc29eee0b62067cf1fdeb60
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-04-09 20:16:24 +0200

    add bg to the content section

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,39 @@ import pageLogo from './images/page-logo.jpeg'
 function App() {
 	return (
 		<div className='font-montse-var'>
-			<section className='relative flex items-center justify-center h-screen overflow-hidden hero-section before:absolute before:-left-10 before:-top-10 before:z-neg before:h-120 before:w-120 before:bg-hero-image before:bg-cover before:bg-center before:shadow-custom-shadow before:blur-sm before:brightness-20'>
-				<div className='fixed w-8 h-8 left-8 top-10'>
+			<section className='flex overflow-hidden relative justify-center items-center h-screen hero-section before:absolute before:-left-10 before:-top-10 before:z-neg before:h-120 before:w-120 before:bg-hero-image before:bg-cover before:bg-center before:shadow-custom-shadow before:blur-sm before:brightness-20'>
+				<div className='fixed left-8 top-10 w-8 h-8'>
 					<img src={pageLogo} alt='logo' />
 				</div>
-				<div className='flex gap-6'>
-					<div className='h-60 w-60'>
-						<img src={musicLogo} alt='music logo' />
+				<div className='flex flex-col gap-10'>
+					<div className='flex gap-6'>
+						<div className='w-60 h-60'>
+							<img src={musicLogo} alt='music logo' />
+						</div>
+						<div className='flex flex-col gap-12 justify-center'>
+							<div>
+								<h1 className='text-4xl font-bold tracking-widest uppercase text-border'>
+									StarJam
+								</h1>
+								<h2 className='text-2xl font-bold text-white uppercase'>
+									Denis
+								</h2>
+							</div>
+							<div className='flex gap-2'>
+								<NormalBtn text='Listen Now' bgColor={true} />
+								<NormalBtn text='Share' bgColor={false} />
+							</div>
+						</div>
 					</div>
-					<div className='flex flex-col justify-center gap-12'>
-						<div>
-							<h1 className='text-4xl font-bold tracking-widest uppercase text-border'>
-								StarJam
-							</h1>
-							<h2 className='text-2xl font-bold text-white uppercase'>Denis</h2>
-						</div>
-						<div className='flex gap-2'>
-							<NormalBtn text='Listen Now' bgColor={true} />
-							<NormalBtn text='Share' bgColor={false} />
-						</div>
+					<div className='flex'>
+						<h3>Stream it your way</h3>
+						<ul>
+							<li>SoundCloud</li>
+						</ul>
 					</div>
 				</div>
 			</section>
-			<section className='content-section'></section>
+			<section className='overflow-hidden min-h-screen bg-center brightness-20 bg-content-image'></section>
 		</div>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        'montse-var': ['Montserrat Variable', 'sans-serif'],
-      },
-      boxShadow: {
-        'custom-shadow': 'inset 0px -120px 30px 15px rgba(0, 0, 0, 0.75)',
-      },
-      backgroundImage: {
-        'hero-image': "url('/src/images/logo2.jpeg')",
-        'content-image':
-          "background-image: url('/src/images/background-tile.webp')",
-      },
-      brightness: {
-        20: '20%',
-      },
-      width: {
-        120: '120%',
-      },
-      height: {
-        120: '120%',
-      },
-      zIndex: {
-        neg: '-1',
-      },
-    },
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'montse-var': ['Montserrat Variable', 'sans-serif'],
+			},
+			boxShadow: {
+				'custom-shadow': 'inset 0px -80px 30px 15px rgba(0, 0, 0, 0.95)',
+			},
+			backgroundImage: {
+				'hero-image': "url('/src/images/logo2.jpeg')",
+				'content-image': "url('/src/images/background-tile.webp')",
+			},
+			brightness: {
+				20: '20%',
+			},
+			width: {
+				120: '120%',
+			},
+			height: {
+				120: '120%',
+			},
+			zIndex: {
+				neg: '-1',
+			},
+		},
+	},
+	plugins: [],
 }
