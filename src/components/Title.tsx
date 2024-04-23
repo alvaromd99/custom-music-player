5790f03c284909fab53c386cd2253ea68dd22751
alvaromd <117214600+alvaromd99@users.noreply.github.com>
2024-04-23 16:23:12 +0200

    change h1 font

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -6,7 +6,7 @@ interface TitleProps {
 export default function Title({ title, subtitle }: TitleProps) {
   return (
     <>
-      <h1 className="text-border text-7xl font-bold mb-4 uppercase tracking-widest">
+      <h1 className="text-border mb-4 font-robo-mono-var text-8xl font-bold uppercase tracking-widest">
         {title}
       </h1>
       <h2 className="text-3xl font-bold uppercase text-white">
