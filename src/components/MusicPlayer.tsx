6c90dd61e4fb602f6566226876c3ae27273fc528
alvaromd <117214600+alvaromd99@users.noreply.github.com>
2024-04-30 18:57:31 +0200

    control the playing in the parent component

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -1,14 +1,35 @@
 import Song from './Song'
 import { tracks } from '../songsData/songData'
+import { useState } from 'react'
 
 export default function MusicPlayer() {
+	const [currentSong, setCurrentSong] = useState<HTMLAudioElement | null>(null)
 
-  return (
-    <div>
-      <h3 className='text-4xl font-bold uppercase mb-10'>Track List</h3>
-      < div className='flex flex-col gap-6' >
-        {tracks.map((track, index) => <Song audio={track.path} number={index} name={track.name} author={track.artist} key={index} />)}
-      </div >
-    </div >
-  )
+	const handlePlay = (audioReference: HTMLAudioElement) => {
+		if (currentSong && currentSong !== audioReference) {
+			currentSong.pause()
+			audioReference.play()
+		} else {
+			currentSong?.pause()
+		}
+		setCurrentSong(audioReference)
+	}
+
+	return (
+		<div>
+			<h3 className="mb-10 text-4xl font-bold uppercase">Track List</h3>
+			<div className="flex flex-col gap-6">
+				{tracks.map((track, index) => (
+					<Song
+						audio={track.path}
+						number={index}
+						name={track.name}
+						author={track.artist}
+						key={index}
+						onPlay={handlePlay}
+					/>
+				))}
+			</div>
+		</div>
+	)
 }
--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -1,71 +1,75 @@
-import { useCallback, useEffect, useRef, useState } from "react"
-import { PauseIcon } from "../icons/PauseIcon"
-import { PlayIcon } from "../icons/PlayIcon"
-import { ShareIcon } from "../icons/ShareIcon"
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { PauseIcon } from '../icons/PauseIcon'
+import { PlayIcon } from '../icons/PlayIcon'
+import { ShareIcon } from '../icons/ShareIcon'
 
 interface SongProps {
-  audio: string
-  number: number
-  name: string
-  author: string
+	audio: string
+	number: number
+	name: string
+	author: string
+	onPlay: (audioReference: HTMLAudioElement) => void
 }
 
-export default function Song({ audio, number, name, author }: SongProps) {
-  const audioRef = useRef<HTMLAudioElement | null>(null)
-  const [isPlaying, setIsPlaying] = useState(false)
-  const [duration, setDuration] = useState<number | undefined>(0)
+export default function Song({
+	audio,
+	number,
+	name,
+	author,
+	onPlay,
+}: SongProps) {
+	const audioRef = useRef<HTMLAudioElement | null>(null)
+	const [isPlaying, setIsPlaying] = useState(false)
+	const [duration, setDuration] = useState<number | undefined>(0)
 
-  const handleLoadedMetaData = useCallback(() => {
-    if (audioRef.current) {
-      setDuration(audioRef.current.duration)
-    }
-  }, [])
+	const handleLoadedMetaData = useCallback(() => {
+		if (audioRef.current) {
+			setDuration(audioRef.current.duration)
+		}
+	}, [])
 
-  useEffect(() => {
-    const audioElement = audioRef.current
-    if (!audioElement) return
+	useEffect(() => {
+		const audioElement = audioRef.current
+		if (!audioElement) return
 
-    audioElement.addEventListener("loadedmetadata", handleLoadedMetaData);
+		audioElement.addEventListener('loadedmetadata', handleLoadedMetaData)
 
-    return () => {
-      audioElement.removeEventListener("loadedmetadata", handleLoadedMetaData)
-    }
-  }, [handleLoadedMetaData])
+		return () => {
+			audioElement.removeEventListener('loadedmetadata', handleLoadedMetaData)
+		}
+	}, [handleLoadedMetaData])
 
-  const handleClick = () => {
-    if (!audioRef.current) return
-    audioRef.current.volume = 0.2
-
-    if (isPlaying) {
-      audioRef.current.pause()
-    } else {
-      audioRef.current.play()
-    }
-    setIsPlaying(!isPlaying)
-  }
-
-  return (
-    <div className="flex justify-between">
-      <audio src={audio} ref={audioRef} />
-      <div className="flex items-center gap-6">
-        <p>
-          {number + 1}
-        </p>
-        <div onClick={handleClick}>{isPlaying ? <PauseIcon dimensions="1.4em" /> : <PlayIcon dimensions="1.4em" />}</div>
-        <div>
-          <p className="font-bold">{name}</p>
-          <p>{author}</p>
-        </div>
-      </div>
-      <div className="flex items-center gap-4">
-        <div>
-          <div>
-            {duration !== undefined ? (duration / 60).toFixed(2) : '0'}</div>
-        </div>
-        <div>
-          <ShareIcon dimensions="1.4em" />
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className="flex justify-between">
+			<audio src={audio} ref={audioRef} />
+			<div className="flex gap-6 items-center">
+				<p>{number + 1}</p>
+				<div
+					onClick={() => {
+						if (audioRef.current) {
+							onPlay(audioRef.current)
+						}
+					}}
+				>
+					{isPlaying ? (
+						<PauseIcon dimensions="1.4em" />
+					) : (
+						<PlayIcon dimensions="1.4em" />
+					)}
+				</div>
+				<div>
+					<p className="font-bold">{name}</p>
+					<p>{author}</p>
+				</div>
+			</div>
+			<div className="flex gap-4 items-center">
+				<div>
+					<div>{duration !== undefined ? (duration / 60).toFixed(2) : '0'}</div>
+				</div>
+				<div>
+					<ShareIcon dimensions="1.4em" />
+				</div>
+			</div>
+		</div>
+	)
 }
