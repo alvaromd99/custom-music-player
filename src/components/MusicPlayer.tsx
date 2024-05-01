5242701b0c867a84f646134a6bdb0a70b5c86b06
alvaromd <117214600+alvaromd99@users.noreply.github.com>
2024-05-01 12:57:50 +0200

    fix playing problems

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -3,42 +3,47 @@ import { tracks } from '../constants/songData'
 import { useState } from 'react'
 
 export default function MusicPlayer() {
-  const [currentSong, setCurrentSong] = useState<HTMLAudioElement | null>(null)
-  const [isPlaying, setIsPlaying] = useState(false)
+	const [currentSong, setCurrentSong] = useState<HTMLAudioElement | null>(null)
+	const [isPlaying, setIsPlaying] = useState(false)
 
-  const handlePlay = (audioReference: HTMLAudioElement) => {
-    if (currentSong && currentSong !== audioReference) {
-      currentSong.pause()
-      audioReference?.play()
-      setIsPlaying(true)
-      setCurrentSong(audioReference)
-    } else if (currentSong && currentSong === audioReference && !isPlaying) {
-      audioReference.play()
-      setIsPlaying(true)
-    }
-    else {
-      currentSong?.pause()
-      setIsPlaying(false)
-    }
-  }
+	const handlePlay = (audioReference: HTMLAudioElement) => {
+		if (!audioReference) return
 
-  return (
-    <div>
-      <h3 className="mb-10 text-4xl font-bold uppercase">Track List</h3>
-      <div className="flex flex-col gap-6">
-        {tracks.map((track, index) => (
-          <Song
-            audio={track.path}
-            number={index}
-            name={track.name}
-            author={track.artist}
-            key={index}
-            onPlay={handlePlay}
-            isPlaying={isPlaying}
-            currentSong={currentSong}
-          />
-        ))}
-      </div>
-    </div>
-  )
+		if (!currentSong) {
+			audioReference.play()
+			setIsPlaying(true)
+			setCurrentSong(audioReference)
+		} else if (currentSong && currentSong !== audioReference) {
+			currentSong.pause()
+			audioReference.play()
+			setIsPlaying(true)
+			setCurrentSong(audioReference)
+		} else if (currentSong && currentSong === audioReference && !isPlaying) {
+			audioReference.play()
+			setIsPlaying(true)
+		} else {
+			currentSong.pause()
+			setIsPlaying(false)
+		}
+	}
+
+	return (
+		<div>
+			<h3 className="mb-10 text-4xl font-bold uppercase">Track List</h3>
+			<div className="flex flex-col gap-6">
+				{tracks.map((track, index) => (
+					<Song
+						audio={track.path}
+						number={index}
+						name={track.name}
+						author={track.artist}
+						key={index}
+						onPlay={handlePlay}
+						isPlaying={isPlaying}
+						currentSong={currentSong}
+					/>
+				))}
+			</div>
+		</div>
+	)
 }
