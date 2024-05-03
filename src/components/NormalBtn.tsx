b316ce804da98323437d523c32d550428b55e8a5
alvaromd <117214600+alvaromd99@users.noreply.github.com>
2024-05-03 15:51:36 +0200

    change border width on title

--- src/components/NormalBtn.tsx
+++ src/components/NormalBtn.tsx
@@ -23,8 +23,8 @@ export default function NormalBtn({ text }: NormalBtnProps) {
       className='flex gap-2 items-center px-12 py-4 text-xl text-center text-white bg-green-400 transition-all duration-300 outline outline-1 hover:bg-white hover:text-black hover:outline-white outline-green-400'
       onClick={handleClick}
     >{isPlaying && currentSong === firstSong ? (
-      <p><PauseIcon dimensions="1.4em" /></p>) : (
-      <p><PlayIcon dimensions="1.4em" /></p>
+      <p><PauseIcon dimensions="1em" /></p>) : (
+      <p><PlayIcon dimensions="1em" /></p>
     )}
       <p>{text}</p>
     </button>
--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,49 +1,48 @@
 import NormalBtn from './NormalBtn'
 import Title from './Title'
-import { PlayIcon } from '../icons/PlayIcon'
 import { ShareIcon } from '../icons/ShareIcon'
 import musicLogo from '../images/logo2.jpeg'
 import { useState } from 'react'
 import ShareBtn from './ShareBtn'
 
 export default function Presentation() {
-	const [copySuccess, setCopySuccess] = useState(false)
+  const [copySuccess, setCopySuccess] = useState(false)
 
-	const copyToClipboard = async (text: string) => {
-		try {
-			await navigator.clipboard.writeText(text)
-			setCopySuccess(true)
-			setTimeout(() => setCopySuccess(false), 3000)
-		} catch (err) {
-			setCopySuccess(false)
-		}
-	}
+  const copyToClipboard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text)
+      setCopySuccess(true)
+      setTimeout(() => setCopySuccess(false), 3000)
+    } catch (err) {
+      setCopySuccess(false)
+    }
+  }
 
-	console.log(copySuccess)
+  console.log(copySuccess)
 
-	return (
-		<div className='flex gap-16 justify-between'>
-			<div className='h-imageBig w-imageBig'>
-				<img src={musicLogo} alt='music logo' />
-			</div>
-			<div className='flex flex-col gap-12 justify-center'>
-				<div>
-					<Title title='NCS' subtitle='No Copyright Sound' />
-				</div>
-				<div className='flex gap-2'>
-					<NormalBtn text='Listen Now' icon={<PlayIcon dimensions='1em' />} />
-					<ShareBtn
-						text='Share'
-						icon={<ShareIcon dimensions='1em' />}
-						handleClick={copyToClipboard}
-					/>
-				</div>
-			</div>
-			{copySuccess && (
-				<div className='absolute top-3 right-3 p-2 text-black bg-white opacity-0 animate-appear'>
-					Text has been copied
-				</div>
-			)}
-		</div>
-	)
+  return (
+    <div className='flex gap-16 justify-between'>
+      <div className='h-imageBig w-imageBig'>
+        <img src={musicLogo} alt='music logo' />
+      </div>
+      <div className='flex flex-col gap-12 justify-center'>
+        <div>
+          <Title title='NCS' subtitle='No Copyright Sound' />
+        </div>
+        <div className='flex gap-2'>
+          <NormalBtn text='Listen Now' />
+          <ShareBtn
+            text='Share'
+            icon={<ShareIcon dimensions='1em' />}
+            handleClick={copyToClipboard}
+          />
+        </div>
+      </div>
+      {copySuccess && (
+        <div className='absolute top-3 right-3 p-2 text-black bg-white opacity-0 animate-appear'>
+          Text has been copied
+        </div>
+      )}
+    </div>
+  )
 }
