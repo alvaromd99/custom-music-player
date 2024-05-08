087b2908a33cbee75ea676fb602e4ab299f6ec94
alvaromd <117214600+alvaromd99@users.noreply.github.com>
2024-05-08 16:14:22 +0200

    change and add icons for soundcloud, ig and facebook

--- src/components/LinksList.tsx
+++ src/components/LinksList.tsx
@@ -1,4 +1,6 @@
-import { AppleIcon } from "../icons/AppleIcon";
+import { FacebookIcon } from "../icons/FacebookIcon";
+import { InstagramIcon } from "../icons/InstagramIcon";
+import { SoundcloudIcon } from "../icons/SoundcloudIcon";
 import { SpotifyIcon } from "../icons/SpotifyIcon";
 import { YoutubeIcon } from "../icons/YoutubeIcon";
 
@@ -12,13 +14,29 @@ export default function LinksList({ outline, column, dimensions }: LinksListProp
   return (
     <ul className={`flex ${column ? 'flex-col' : ''}`}>
       <li className={`flex justify-center items-center w-16 h-16 ${outline ? 'outline-white outline-1 outline' : ''}`}>
-        <AppleIcon dimensions={`${dimensions}em`} />
+        <a href="https://open.spotify.com/playlist/7sZbq8QGyMnhKPcLJvCUFD?si=a4cf1983d05b4027&nd=1&dlsi=885e344ddd284839" target="_blank">
+          <SpotifyIcon dimensions={`${dimensions}em`} />
+        </a>
       </li>
       <li className={`flex justify-center items-center w-16 h-16 ${outline ? 'outline-white outline-1 outline' : ''}`}>
-        <SpotifyIcon dimensions={`${dimensions}em`} />
+        <a href="https://www.youtube.com/nocopyrightsounds">
+          <YoutubeIcon dimensions={`${dimensions}em`} />
+        </a>
       </li>
       <li className={`flex justify-center items-center w-16 h-16 ${outline ? 'outline-white outline-1 outline' : ''}`}>
-        <YoutubeIcon dimensions={`${dimensions}em`} />
+        <a href="https://soundcloud.com/nocopyrightsounds">
+          <SoundcloudIcon dimensions={`${dimensions}em`} />
+        </a>
+      </li>
+      <li className={`flex justify-center items-center w-16 h-16 ${outline ? 'outline-white outline-1 outline' : ''}`}>
+        <a href="https://www.instagram.com/nocopyrightsounds/">
+          <InstagramIcon dimensions={`${dimensions}em`} />
+        </a>
+      </li>
+      <li className={`flex justify-center items-center w-16 h-16 ${outline ? 'outline-white outline-1 outline' : ''}`}>
+        <a href="https://www.facebook.com/NoCopyrightSounds/">
+          <FacebookIcon dimensions={`${dimensions}em`} />
+        </a>
       </li>
     </ul >
   )
