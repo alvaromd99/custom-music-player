cd867b471477dc59a17738c2565a8fb1474a0a4b
alvaromd <117214600+alvaromd99@users.noreply.github.com>
2024-05-10 10:43:26 +0200

    make listicon comp and use it in the linkslist

--- src/components/LinksList.tsx
+++ src/components/LinksList.tsx
@@ -3,6 +3,7 @@ import { InstagramIcon } from "../icons/InstagramIcon";
 import { SoundcloudIcon } from "../icons/SoundcloudIcon";
 import { SpotifyIcon } from "../icons/SpotifyIcon";
 import { YoutubeIcon } from "../icons/YoutubeIcon";
+import ListIcon from "./ListIcon";
 
 interface LinksListProps {
   outline: boolean,
@@ -13,31 +14,11 @@ interface LinksListProps {
 export default function LinksList({ outline, dimensions, isHeader }: LinksListProps) {
   return (
     <ul className={`flex ${isHeader ? 'flex-row gap-7 lg:flex-col' : ''}`}>
-      <li className={`flex justify-center items-center w-10 h-10 z-20 ${outline ? 'outline-white outline-1 outline' : ''}`}>
-        <a href="https://open.spotify.com/playlist/7sZbq8QGyMnhKPcLJvCUFD?si=a4cf1983d05b4027&nd=1&dlsi=885e344ddd284839" target="_blank">
-          <SpotifyIcon dimensions={`${dimensions}em`} />
-        </a>
-      </li>
-      <li className={`flex justify-center items-center w-10 h-10 z-20 ${outline ? 'outline-white outline-1 outline' : ''}`}>
-        <a href="https://www.youtube.com/nocopyrightsounds" target="_blank">
-          <YoutubeIcon dimensions={`${dimensions}em`} />
-        </a>
-      </li>
-      <li className={`flex justify-center items-center w-10 h-10 z-20 ${outline ? 'outline-white outline-1 outline' : ''}`}>
-        <a href="https://soundcloud.com/nocopyrightsounds" target="_blank">
-          <SoundcloudIcon dimensions={`${dimensions}em`} />
-        </a>
-      </li>
-      <li className={`flex justify-center items-center w-10 h-10 z-20 ${outline ? 'outline-white outline-1 outline' : ''}`}>
-        <a href="https://www.instagram.com/nocopyrightsounds/" target="_blank">
-          <InstagramIcon dimensions={`${dimensions}em`} />
-        </a>
-      </li>
-      <li className={`flex justify-center items-center w-10 h-10 z-20 ${outline ? 'outline-white outline-1 outline' : ''}`}>
-        <a href="https://www.facebook.com/NoCopyrightSounds/" target="_blank">
-          <FacebookIcon dimensions={`${dimensions}em`} />
-        </a>
-      </li>
+      <ListIcon outline={outline} icon={<SpotifyIcon dimensions={`${dimensions}em`} />} />
+      <ListIcon outline={outline} icon={<YoutubeIcon dimensions={`${dimensions}em`} />} />
+      <ListIcon outline={outline} icon={<SoundcloudIcon dimensions={`${dimensions}em`} />} />
+      <ListIcon outline={outline} icon={<InstagramIcon dimensions={`${dimensions}em`} />} />
+      <ListIcon outline={outline} icon={<FacebookIcon dimensions={`${dimensions}em`} />} />
     </ul >
   )
 }
